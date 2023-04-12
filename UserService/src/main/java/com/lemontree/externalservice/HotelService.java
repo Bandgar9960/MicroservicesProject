6b7ad0c97885179be692fa2fedc0c1436185d4ff
Dnyaneshwar Bandgar <Bandgar9960@gmail.com>
2023-04-12 18:39:43 +0530

    Config Server Github Done...Testing Remaining

---
+++ UserService/src/main/java/com/lemontree/externalservice/HotelService.java
@@ -0,0 +1,13 @@
+package com.lemontree.externalservice;
+
+import com.lemontree.model.Hotel;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@FeignClient(name = "HOTEL-SERVICE")
+public interface HotelService {
+
+    @GetMapping("/hotels/{hotelId}")
+    Hotel getHotel(@PathVariable Long hotelId);
+}
