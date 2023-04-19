9a8559fc677e221658e27524968186340c363e05
Dnyaneshwar Bandgar <Bandgar9960@gmail.com>
2023-04-19 13:45:54 +0530

    FeignClientInterceptor and RestTemplateInterceptor Implementation Done

--- UserService/src/main/java/com/lemontree/config/MyConfig.java
+++ UserService/src/main/java/com/lemontree/config/MyConfig.java
@@ -1,11 +1,13 @@
 package com.lemontree.config;
 
+import com.lemontree.config.intercepter.RestTemplateInterceptor;
 import com.nimbusds.oauth2.sdk.client.ClientRegistrationRequest;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.ClientHttpRequestInterceptor;
 import org.springframework.security.oauth2.client.OAuth2AuthorizedClientManager;
 import org.springframework.security.oauth2.client.OAuth2AuthorizedClientProvider;
 import org.springframework.security.oauth2.client.OAuth2AuthorizedClientProviderBuilder;
@@ -13,6 +15,9 @@ import org.springframework.security.oauth2.client.registration.ClientRegistratio
 import org.springframework.security.oauth2.client.web.DefaultOAuth2AuthorizedClientManager;
 import org.springframework.security.oauth2.client.web.OAuth2AuthorizedClientRepository;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Configuration
 public class MyConfig {
@@ -25,6 +30,15 @@ public class MyConfig {
     @Bean
     @LoadBalanced
     public RestTemplate restTemplate() {
+
+        RestTemplate restTemplate = new RestTemplate();
+        List<ClientHttpRequestInterceptor> interceptors = new ArrayList<>();
+
+        interceptors.add(new RestTemplateInterceptor(manager(clientRegistrationRepository, oAuth2AuthorizedClientRepository)));
+
+        restTemplate.setInterceptors(interceptors);
+
+
         return new RestTemplate();
     }
 
